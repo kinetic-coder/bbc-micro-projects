1eddead0bb6b2af38fc7911738b0930aa0ebef52
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2021-12-28 17:24:38 +0000

    Updated to clear screen

--- main.ts
+++ main.ts
@@ -1,18 +1,26 @@
 input.onGesture(Gesture.FreeFall, function () {
     basic.showString("GO")
     radio.sendString("MOVE-FORWARD")
+    basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showString("L")
     radio.sendString("TURN-LEFT")
+    basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showString("STOP")
     radio.sendString("STOP")
+    basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltRight, function () {
     basic.showString("R")
     radio.sendString("TURN-RIGHT")
+    basic.pause(1000)
+    basic.clearScreen()
 })
 radio.setGroup(50)
 basic.forever(function () {
