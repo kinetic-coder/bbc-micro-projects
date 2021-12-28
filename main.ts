c0ce764e502bb4fb8fbcd0288a50010942ad26f6
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2021-12-28 18:29:18 +0000

    Updated messages sent

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("G")
-    basic.showString("GO")
+    basic.showString("G")
 })
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendString("<")
     basic.showString("<")
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendString("STOP")
+    radio.sendString("S")
     basic.showString("S")
 })
 input.onGesture(Gesture.TiltRight, function () {
